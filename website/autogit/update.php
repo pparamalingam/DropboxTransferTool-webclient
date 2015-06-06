9bf57cb2910c7aa60f30fab012ccf670de07c115
Preshoth Paramalingam <preshoth@gmail.com>
2015-06-06 18:08:07 -0400

    tests

--- website/autogit/update.php
+++ website/autogit/update.php
@@ -2,7 +2,7 @@
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
+if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
   shell_exec( 'cd /var/www/testdbsite/DropboxTransferTool-webclient && git reset --hard HEAD && git pull' );
 }
 
