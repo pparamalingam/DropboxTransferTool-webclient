67df4e03d1baac8a29aeea0d4c0982bef3ca1e58
Matthew Francis <matthew_francis@outlook.com>
2015-06-06 16:45:20 -0400

    add google sign in button and improve website styling

--- website/js/helper.js
+++ website/js/helper.js
@@ -19,3 +19,10 @@
         if(jsonLinks.items.length >0){
           JSON.stringify(jsonLinks);
         }
+//var url = window.location.pathname;
+var url = 'https://accounts.google.com/o/oauth2/auth?response_type=token&client_id=662147109054-0k37pvm818fjdr06emtuldcsaihlde9i.apps.googleusercontent.com&redirect_uri=https:localhost:8001=https://picasaweb.google.com/data/'
+var token = url.substring(url.lastIndexOf('id=')+3);
+var urlJunk = token.substring(token.lastIndexOf('.apps')).length;
+token = token.slice(0,token.length-urlJunk);
+console.log(token);
+
