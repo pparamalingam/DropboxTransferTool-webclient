00efa2e7ed97301feeb4130c337af86b8b4ec578
Preshoth Paramalingam <preshoth@gmail.com>
2015-06-06 17:40:16 -0400

    webhook

--- website/autogit/update.php
+++ website/autogit/update.php
@@ -1,26 +1,9 @@
 <?php
-function cidr_match($ip, $ranges)
-{
-    $ranges = (array)$ranges;
-    foreach($ranges as $range) {
-        list($subnet, $mask) = explode('/', $range);
-        if((ip2long($ip) & ~((1 << (32 - $mask)) - 1)) == ip2long($subnet)) {
-            return true;
-        }
-    }
-    return false;
+
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
 }
 
-$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
-$github_cidrs = array('204.232.175.64/27', '192.30.252.0/22');
-
-if(in_array($_SERVER['REMOTE_ADDR'], $github_ips) || cidr_match($_SERVER['REMOTE_ADDR'], $github_cidrs)) {
-    $dir = '/var/www/testdbsite/DropboxTransferTool-webclient';
-    exec("cd $dir && git pull 2>&1", $output);
-    echo $output;
-}
-else {
-    header('HTTP/1.1 404 Not Found');
-    echo '404 Not Found.';
-    exit;
-}
+?>
