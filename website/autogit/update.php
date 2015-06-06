6dfba111c656a341cbdf7b2d0c178913b0424e97
Preshoth Paramalingam <preshoth@gmail.com>
2015-06-06 17:51:48 -0400

    webhook

--- website/autogit/update.php
+++ website/autogit/update.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+  shell_exec( 'cd /var/www/testdbsite/DropboxTransferTool-webclient && git reset --hard HEAD && git pull' );
 }
 
 ?>
