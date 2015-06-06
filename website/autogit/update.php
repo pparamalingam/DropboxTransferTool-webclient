e027c2c44444d31a290f0d830410e78cafaefcb7
Preshoth Paramalingam <preshoth@gmail.com>
2015-06-06 18:16:56 -0400

    revert hook

--- website/autogit/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  shell_exec( 'cd /var/www/testdbsite/DropboxTransferTool-webclient && git reset --hard HEAD && git pull' );
-}
-
-?>
